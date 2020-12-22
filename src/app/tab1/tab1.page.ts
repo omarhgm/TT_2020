7fba4daa48ac091c080f539542523a176424324d
omarhgm <omarhgm73@gmail.com>
2020-12-22 13:15:41 -0600

    Prueba de la libreria mapbox funcionando

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  ionViewDidEnter() {
+    this.buildMap();
+  }
 
-  constructor() {}
+constructor() { }
+
+buildMap() {
+  mapboxgl.accessToken = 'pk.eyJ1Ijoib21hcjc3IiwiYSI6ImNrY3ZkNXVuYTAzNHMzMG5yeDJmaWp1ODUifQ.spAq7A_MdqTWtGkdKo8B_A';
+  const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+  });
+}
 
 }
